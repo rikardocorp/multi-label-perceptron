24a05e0b2a20fff5949149cdb8b1bc7104977538
Ricardo Coronado <rikardo.corp@gmail.com>
2016-04-27 12:25:22 -0500

    documentando

--- main.cpp
+++ main.cpp
@@ -12,7 +12,11 @@
 #include <fstream>
 #include <string>
 #include <math.h>
+#include <ctime>
+
 using namespace std;
+
+//  ESTRUCTURA PARA DATA DE ENTRENAMIENTO Y TEST
 
 struct _datos
 {
@@ -21,6 +25,8 @@ struct _datos
     struct _datos *sig = NULL;
 };
 
+//  FUNCIONES
+
 void obtener_data(string,int);
 void liberar_memoria();
 void imprimir_array();
@@ -28,11 +34,11 @@ void mlp();
 void mlp_test();
 void copiar_input(_datos *origen, int cantidad);
 float f_signoid(float numero);
-void multiplicacion_punto(int pos_inicial,int pos_sig,int capa1, int capa2);
+void multiplicacion_punto(int ,int ,int , int );
 void generar_deltas(int pos_target,int pos, int capaAux);
 void actualizar_pesos(int posInput, int posDelta, int cant_input, int cant_deltas, int numeroCapa);
 
-
+//  VARIABLES GLOBALES
 
 _datos *point_data_fit,*point_data_test;
 float *point_capa;
@@ -45,10 +51,6 @@ int *pos_ini;
 int total_array;
 float learn_rate;
 float **sumatorias_error;
-//float target[3][3] ={{ 0.99,0.01,0.01},
-//                { 0.01,0.99,0.01},
-//                { 0.01,0.01,0.99}};
-
 float target[8][8] = {{0.99, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01},
                     {0.01, 0.99, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01},
                     {0.01, 0.01, 0.99, 0.01, 0.01, 0.01, 0.01, 0.01},
@@ -58,7 +60,13 @@ float target[8][8] = {{0.99, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01},
                     {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.99, 0.01},
                     {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.99}};
 //float target[2][2] ={{0.99,0.01},{0.01,0.99}};
+//float target[3][3] ={{0.99,0.01,0.01},{0.01,0.99,0.01},{0.01,0.01,0.99}};
 
+
+//  VARIABLES PARA EL CLOCK
+
+clock_t start;
+double duration;
 
 int main(int argc, const char * argv[])
 {
@@ -68,16 +76,18 @@ int main(int argc, const char * argv[])
     
     total_capas = 3;
     neurona = (int *)malloc(sizeof(int)*total_capas+1);
+    
+    //  Estructura de la neurona el indice del array neurona es el numero de capas y el valor es la cantidad de neuronas
     neurona[0] = 58;
-    neurona[1] = 10;
+    neurona[1] = 9;
     neurona[2] = 8;
     neurona[3] = 0;
     total_input = neurona[0];
     total_salidas = neurona[total_capas-1];
+    learn_rate = 0.9;
+    
+    //  Array para almacenar posiciones de inicio de las capas
     pos_ini = (int *)malloc(sizeof(int)*total_capas);
-    sumatorias_error = (float **)malloc(sizeof(float)*(total_capas-1));
-    learn_rate = 0.5;
-    srand(time(0));
 
     //  inicializamos las posiciones de las capas - y hallamos el total de elementos del array
     int total = 0;
@@ -89,12 +99,14 @@ int main(int argc, const char * argv[])
     
     //  inicializamos el array principal de elementos con valores random
     total_array = total;
+    srand(time(0));
     capa = (float *)malloc(sizeof(float)*total_array);
     for (int i = 0; i< total; i++) {
         capa[i] = (double) rand()/RAND_MAX;
     }
     
     //  Reservamos espacio en memoria para una matriz que almacene el error acumulado de una neurona
+    sumatorias_error = (float **)malloc(sizeof(float)*(total_capas-1));
     for (int i = 0; i< total_capas-1; i++) {
         sumatorias_error[i] = (float *)malloc(sizeof(float)*(neurona[i]+1));
         for (int j=0; j < neurona[i]+1; j++) {
@@ -137,10 +149,13 @@ int main(int argc, const char * argv[])
 }
 
 
+//  MLP FORWARD Y BACKWARD PROPAGATION
 void mlp()
 {
-    int total_epocas = 2000;
+    int total_epocas = 3000;
     _datos *data;
+    
+    start = clock();
     
     //  Recorremos las epocas
     for (int epoca = 0; epoca< total_epocas; epoca++)
@@ -149,15 +164,20 @@ void mlp()
         data = point_data_fit;
         while (data != NULL)
         {
-//            point_capa = capa;
+            //  Inicializamos el array con los datos de entrada del caso de entrenamiento
             copiar_input(data,total_input);
             
-            for (int c = 0; c < total_capas-1; c++) {
+            //  FORWARD PROPAGATION
+            for (int c = 0; c < total_capas-1; c++)
+            {
                 multiplicacion_punto(pos_ini[c], pos_ini[c+1], neurona[c], neurona[c+1]);
             }
             
-            for (int c = total_capas-2; c > -1; c--) {
-                if (c == total_capas-2) {
+            //  BACKWARD PROPAGATION
+            for (int c = total_capas-2; c > -1; c--)
+            {
+                if (c == total_capas-2)
+                {
                     generar_deltas(data->target-1, pos_ini[c+1], neurona[c+1]);
                 }
                 actualizar_pesos(pos_ini[c],pos_ini[c+1],neurona[c],neurona[c+1],c);
@@ -166,9 +186,12 @@ void mlp()
             data = data->sig;
         }
     }
-    //    free(data);
+    
+    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
 }
 
+
+//  MLP TEST FORWARD PROPAGATION
 void mlp_test()
 {
     _datos *data = point_data_fit;
@@ -176,6 +199,10 @@ void mlp_test()
     int pos_resultado = pos_ini[total_capas - 1] + 1;
     string salida;
     
+    int contador = 0;
+    int salidaFinal = 0;
+    int contadorCasos = 0;
+    float acurracy = 0;
     
     while (data != NULL) {
         point_capa = capa;
@@ -188,19 +215,33 @@ void mlp_test()
             multiplicacion_punto(pos_ini[c], pos_ini[c+1], neurona[c], neurona[c+1]);
         }
         
-        
+        contadorCasos++;
         for (int i = 0; i < total_salidas; i++)
         {
-            cout<<point_capa[pos_resultado+i]<<" - ";
+            
+            salidaFinal = (int)(point_capa[pos_resultado+i]+0.5);
+            if(res-1 == i && salidaFinal == 1)
+            {
+                contador++;
+            }
+            cout<<salidaFinal<<" - ";
         }
         
         cout<<"- "<<res<<endl;
         data = data->sig;
     }
+    
+    acurracy = contador * 100 / contadorCasos;
+    cout<<endl<<endl<<"Tiempo de Entrenamiento: "<< duration <<endl;
+    cout<<"Total de Casos: "<<contadorCasos<<endl;
+    cout<<"Aciertos: "<<contador<<endl;
+    cout<<"Desaciertos: "<<contadorCasos - contador<<endl;
+    cout<<"Accuracy: "<<acurracy<<"%"<<endl;
+    
 }
 
 
-//  Inicializamos las entradas para la primera capa
+//  INICIALIZAMOS LAS ENTRADAS PARA LA PRIMERA CAPA DE CADA CASO DE ENTRENAMIENTO
 void copiar_input(_datos *origen, int cantidad)
 {
     point_capa[0] = 1;
@@ -209,7 +250,8 @@ void copiar_input(_datos *origen, int cantidad)
     }
 }
 
-//  Generamos los deltas para la primera iteracion del Backward Propagation
+
+//  GENERAMOS LOS DELTAS PARA LA PRIMERA ITERACION DEL BACKWARD PROPAGATION
 void generar_deltas(int pos_target,int pos, int capaAux)
 {
     for (int i = 1; i < capaAux+1; i++) {
@@ -219,7 +261,7 @@ void generar_deltas(int pos_target,int pos, int capaAux)
     }
 }
 
-//  Actualizar pesos
+//  ACTUALIZAR PESOS
 void actualizar_pesos(int posInput, int posDelta, int cant_input, int cant_deltas,int numeroCapa){
     // indicie para marcar el inicio de la seccion de pesos de la capa i
     int ini_matriz = posInput + cant_input + 1;
@@ -254,34 +296,35 @@ void actualizar_pesos(int posInput, int posDelta, int cant_input, int cant_delta
     }
 }
 
-void imprimir_array()
-{
-    for (int i = 0; i < total_array; i++) {
-        cout<<point_capa[i]<<"|";
-    }
 
-}
-
-void multiplicacion_punto(int pos_inicial,int pos_sig,int capa1, int capa2){
+//  SIMULACION DE MULTIPLICACION DE MATRICES
+    //  pos_input -> manda la posicion inicial de las input para la capa
+    //  pos_peso -> manda la posicion inicial de los pesos
+    //  capa_iniput -> cantidad de Inputs que alimentan a la capa actual = a la cantidad de neuronas de la capa anterior
+    //  capa_peso -> cantidad de pesos que recibe la neurona de la capa actual
+void multiplicacion_punto(int pos_input,int pos_peso,int capa_input, int capa_peso){
     float sumatoria;
-    int ini_matriz = capa1 + 1;
-    point_capa[pos_sig] = 1;
+    int ini_matriz = capa_input + 1;
+    point_capa[pos_peso] = 1;
 
-    for (int i=1; i<capa2+1; i++){
+    for (int i=1; i<capa_peso+1; i++){
         sumatoria = 0;
-        for (int j=0; j<capa1+1; j++){
-            sumatoria = sumatoria + point_capa[pos_inicial+j]* point_capa[pos_inicial+ini_matriz*i+j];
+        for (int j=0; j<capa_input+1; j++){
+            sumatoria = sumatoria + point_capa[pos_input+j]* point_capa[pos_input+ini_matriz*i+j];
         }
-        point_capa[pos_sig+i] = f_signoid(sumatoria);
+        point_capa[pos_peso+i] = f_signoid(sumatoria);
     }
 }
 
 
+//  FUNCION DE ACTIVACION
 float f_signoid(float numero)
 {
     return 1 / (1 + pow(exp(1), -1 * numero));
 }
 
+
+//  GENERAMOS UNA ESTRUCTURA CON LA DATA DE ENTRENAMIENTO O TEST
 void obtener_data(string archivo,int total_input){
     
     string numero;
@@ -350,6 +393,14 @@ void liberar_memoria()
     free(puntero_aux);
     free(puntero_aux2);
 }
+
+void imprimir_array()
+{
+    for (int i = 0; i < total_array; i++) {
+        cout<<point_capa[i]<<"|";
+    }
+}
+
 
 
 
